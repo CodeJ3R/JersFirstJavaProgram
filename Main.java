af6813a66d7fcfe7f6a875795e09ba18efb1ec67
Jer <CodeJ3r@gmail.com>
2025-01-08 11:03:36 -0400

    Adding My First Java Program!

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*
+ * Name:Jeremy Paruch
+ * Student Number: W0222971
+ * Date January 07, 2025
+ * Instructor: Nadia Gouda
+ * Class: PROG 1400 - Introduction to Object-Oriented Programming
+ */
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
