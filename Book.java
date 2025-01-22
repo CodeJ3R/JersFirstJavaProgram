e0ceb1ceb3168d84719b4593f67a64f7411c7abf
Jer <CodeJ3r@gmail.com>
2025-01-22 16:45:47 -0400

    Exercise 3: Using Constructors in Java
    Adding Constructors to the Java file I made in Exercise 2

--- Book.java
+++ Book.java
@@ -1,11 +1,12 @@
 /*
  * Name:Jeremy Paruch
  * Student Number: W0222971
- * Date January 13, 2025
+ * Date January 22, 2025
  * Instructor: Nadia Gouda
  * Class: PROG 1400 - Introduction to Object-Oriented Programming
+ * Exercise 3: Using Constructors in Java
  */
-// Class created step 2!
+// Class created from Exercise 2 step 2!
 public class Book {
     // title String is set to private based on the instructions of the assignment
     private String title;
@@ -13,28 +14,100 @@ public class Book {
     private int published;
     // author string is set to private based on the instrutions of the assignment
     private String author;
+    // A boolean variable to show if I own the book. It's set to false first to show which books I dont own
+    private boolean owned=false;
 
     // These are the getters set up for the main 
 
     public String getTitle() {
-        return this.title;
+        return title;
     }
     public int getPublished() {
-        return this.published;
+        return published;
     }
     public String getAuthor() {
-        return this.author;
+        return author;
+    }
+    public Boolean getOwned(){
+        return owned;
     }
 
     // These are the setters for the main
-    public void setTitle(String newTitle) {
-        this.title = newTitle;
+    public void setTitle(String title) {
+        this.title = title;
     }
-    public void setPublished(int newPublished) {
-        this.published = newPublished;
+    public void setPublished(int published) {
+        this.published = published;
     }    
         
-    public void setAuthor(String newAuthor){
-        this.author = newAuthor;
-    }    
-}
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setOwned(Boolean owned){
+        this.owned = owned;
+    }
+
+    // A constructor but with no arguments aka the default state!
+     public Book () {
+     this.title = "There's No Author listed";
+     this.published = 0;
+     this.author = "I can't find an author...";
+     this.owned = owned;
+     }
+
+     // Method Overloading!
+
+     // Method with ONE Parameter!
+     public Book (String t){
+     this.title = t;
+     this.published = 0;
+     this.author ="I cant find an author";
+     this.owned = owned;
+     } 
+      // Method with TWO Parameters!
+     public Book (String t, int p){
+        this.title = t;
+        this.published =p;
+        this.author ="I cant find an author";
+        this.owned = owned;
+     }
+     // Method with THREE Parameters!
+     public Book (String t, int p, String a){
+        this.title = t;
+        this.published =p;
+        this.author = a;
+        this.owned = owned;
+    } 
+     // Method with FOUR Parameters!    
+    public Book (String t, int p, String a, boolean o ){
+        this.title = t;
+        this.published =p;
+        this.author = a;
+        this.owned = !owned;
+    }
+        // Display method for the parameters
+        public void display() {
+        System.out.println(" The Title of this Book is : "  + this.title);
+        System.out.println(" The year published is in: "  + this.published);
+        System.out.println(" The Author for this book is: "  + this.author );
+        System.out.println(" DO I own this book? "  + this.owned  );
+    }
+}    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Main.java
+++ Main.java
@@ -1,13 +1,44 @@
 /* 
  * Name:Jeremy Paruch
  * Student Number: W0222971
- * Date January 13, 2025
+ * Date January 22, 2025
  * Instructor: Nadia Gouda
  * Class: PROG 1400 - Introduction to Object-Oriented Programming
+ * Exercise 3: Using Constructors in Java
  */
 
 public class Main{
     public static void main(String[] args) {
+        Book myInspiration = new Book();
+        Book dmGuide = new Book("Dungeons Master Guide 2024");
+        Book playerHandBook = new Book ("Players HandBook", 2014);
+        Book myPythonBook = new Book("Python Crash Course",2023,"Eric Matthes");
+        Book favBook = new Book("How To Win At Chess",2023,"Levy Rozman",true);
+
+        System.out.println("******** Here is a Display for my  initial values BUT with no arguments**********\n");
+        myInspiration.display();
+
+        System.out.println("******** Here is a Display for initial values BUT there's ONE parameters **********\n");
+        dmGuide.display();
+  
+        System.out.println("********Here is a Display for initial values BUT there's TWO parameters **********\n");
+        playerHandBook.display();
+  
+        System.out.println("********Here is a Display for initial values BUT there's THREE parameters **********\n");
+        myPythonBook.display();
+
+        System.out.println("********Here is a Display for initial values BUT there's FOUR parameters**********\n");   
+        favBook.display();
+  
+        System.out.println("********Horray! We made it to the end! #Final Fantasy 7 victory music.mp3# **************" );
+    
+
+
+        
+    }
+}
+
+        /* Code from Exercise 2 
         // This is my favourite book! Also this is the object created for step 3
         Book favBook = new Book();
         //This is the title of the book
@@ -23,10 +54,7 @@ public class Main{
         System.out.println(favBook.getPublished());
         // A getter for...getting the author of the book!
         System.out.println(favBook.getAuthor());
-    }
-}
-
-
+        */
 
 
 /*    public static void main(String[] args) {
